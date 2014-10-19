2e74968c984579ba15b3a757f070b4676e1f1b7e
scompo <scompo@localhost>
2014-10-19 20:37:42 +0200

    added tester and tests

---
+++ src/main/java/org/scompo/scompotestjava/Tester.java
@@ -0,0 +1,23 @@
+package org.scompo.scompotestjava;
+
+/**
+ * Stupid tester
+ */
+public class Tester {
+
+	private Tester(){
+		
+	}
+	
+	/**
+	 * Returns what you pass on..
+	 * 
+	 * @param pass that's what you get.
+	 * 
+	 * @return what you passed.
+	 */
+	public static boolean test(boolean pass){
+		
+		return pass;
+	}
+}
---
+++ src/test/java/org/scompo/scompotestjava/test/TestRandom.java
@@ -0,0 +1,25 @@
+package org.scompo.scompotestjava.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.scompo.scompotestjava.Tester;
+
+public class TestRandom {
+
+	/**
+	 * This test always passes! 
+	 */
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void testerTest(){
+		
+		assertTrue(Tester.test(true));
+		assertFalse(Tester.test(false));
+	}
+
+}
